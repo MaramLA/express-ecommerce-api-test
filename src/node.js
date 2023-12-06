2278aefa58a949a3a02b0296796b4d6d111439ee
MaramLA <maramalfaraj2000@gmail.com>
2023-12-06 13:37:34 +0300

    added new point

--- src/node.js
+++ src/node.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const app = express();
 
+let products = [
+  { id: 1, title: "iphone 11", price: 1100 },
+  { id: 2, title: "iphone 12", price: 1200 },
+  { id: 3, title: "iphone 13", price: 1300 },
+  { id: 4, title: "iphone 14", price: 1400 },
+];
+
 const port = 3004;
+
+app.get("/", (request, response) => {
+  response.send("Welcome to the express server");
+});
 
 app.get("/products", (request, response) => {
   response.send({
-    products: [
-      { id: 1, title: "iphone 11", price: 1100 },
-      { id: 2, title: "iphone 12", price: 1200 },
-      { id: 3, title: "iphone 13", price: 1300 },
-      { id: 4, title: "iphone 14", price: 1400 },
-    ],
+    products: products,
+  });
+});
+
+app.get("/products/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const product = products.find((product) => product.id === id);
+  response.send({
+    product: product,
   });
 });
 
