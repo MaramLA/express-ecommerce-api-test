89b22040d2893a5f88b4cc8b8464bfcf546de37c
MaramLA <maramalfaraj2000@gmail.com>
2023-12-10 19:12:12 +0300

    update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(
+      process.env.MONGODB_URI ||
+        "mongodb+srv://lsmrnr6:L7MhQj2pOrZNiqBn@cluster0.xbtcmjt.mongodb.net/full-stack-demo-db"
+    );
     console.log("database is connectd");
   } catch (error) {
     console.log(error.message);
